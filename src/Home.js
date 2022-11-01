ecee7ee06951f80dedcf17d1e73628b24e24b172
Christian Espinoza <--global>
2022-11-01 11:47:38 -0500

    Outputting Lists

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,20 @@
 import {useState} from 'react'
 
 const Home = () => {
-     
-        const [name,setName]= useState('mario')
-        const [age,setAge]= useState(50)
-        const handleClick = () =>{
-            setName('luigi')
-            setAge(30)
-            console.log(name)
-        }
+    const [blogs,SetBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
 
     return (
         <div className="home">
-            <h2>App component</h2>
-            <p>{name} {age}</p>
-            <button onClick={handleClick}>click</button>
+            {blogs.map((blog)=>(
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>written by {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 }
