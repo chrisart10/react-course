891e29b5abb634a3c66c633a06ea726c389cb3a1
Christian Espinoza <--global>
2022-11-01 10:26:16 -0500

    Add content Dynamic content in App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog'
+  const likes = 50
+  const person = { userName: 'Yoshi', age: 30 }
+  const link = 'http://www.google.com'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{title} </h1>
+      <p>Liked {likes} times</p>
+      <p>{person.userName} {person.age}</p>
+      <p>{"Hello, ninjas"}</p>
+      <p>{[1,2,3,4,5,6,7,8]}</p>
+      <p>{Math.random()*10}</p>
+      <a href={link}>google</a>
     </div>
   );
 }
