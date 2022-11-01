e260478a691a1501d6a4ce0b0427075223b915bc
Christian Espinoza <--global>
2022-11-01 11:03:08 -0500

    Add events to button

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,18 @@
 const Home = () => {
-    return ( 
+    const handleClick = (e) => {
+        console.log('Hello ninjas',e.target)
+    }
+    const handleClickAgain = (name) => {
+        console.log(name)
+    }
+
+    return (
         <div className="home">
             <h2>App component</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => handleClickAgain('Hello christian')}>Click me Again</button>
         </div>
-     );
+    );
 }
- 
+
 export default Home;
