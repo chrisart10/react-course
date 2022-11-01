0c9ea587961663390902ada2917ca83619ecbafe
Christian Espinoza <--global>
2022-11-01 10:34:39 -0500

    Add components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog'
-  const likes = 50
-  const person = { userName: 'Yoshi', age: 30 }
-  const link = 'http://www.google.com'
   return (
     <div className="App">
-      <h1>{title} </h1>
-      <p>Liked {likes} times</p>
-      <p>{person.userName} {person.age}</p>
-      <p>{"Hello, ninjas"}</p>
-      <p>{[1,2,3,4,5,6,7,8]}</p>
-      <p>{Math.random()*10}</p>
-      <a href={link}>google</a>
+      <Navbar/>
+      <div className="content">
+        <Home/>
+      </div>
     </div>
   );
 }
