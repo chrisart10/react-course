efd675d4543ef77aa91ce189db9aaa55eb1458bc
Christian Espinoza <--global>
2022-11-01 15:34:56 -0500

    Add useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,34 @@
-import {useState} from 'react'
+import { useState, useEffect } from 'react'
 import BlogsList from './Blogs';
 
 const Home = () => {
-    const [blogs,SetBlogs] = useState([
+    const [blogs, SetBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
 
-    const handleDelete = (id) =>{
+    const handleDelete = (id) => {
         const newBlogs = blogs.filter((blog) => blog.id !== id)
         SetBlogs(newBlogs)
     }
 
+    useEffect(()=>{
+        console.log('this code run in useEffect')
+    })
+
     return (
         <div className="home">
-            <BlogsList blogs = {blogs} title="All Blogs"/>
             <BlogsList
-             blogs = {blogs.filter((blog)=> blog.author === "mario")}
-              title="All Mario's Blogs" 
-              handleDelete = {handleDelete}
-              />
+                blogs={blogs}
+                title="All Blogs"
+                handleDelete={handleDelete}
+            />
+            <BlogsList
+                blogs={blogs.filter((blog) => blog.author === "mario")}
+                title="All Mario's Blogs"
+                handleDelete={handleDelete}
+            />
         </div>
     );
 }
