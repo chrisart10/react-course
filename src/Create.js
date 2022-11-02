7d29a52f2b7de55b502abd88e777ea08278b509d
Christian Espinoza <--global>
2022-11-02 15:37:41 -0500

    Add onSubmit and handlerSubmit

--- src/Create.js
+++ src/Create.js
@@ -1,12 +1,49 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+    const [author, setAuthor] = useState('mario')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const blog = { title, body, author }
+        console.log(blog)
+    }
     return (
         <div className="create">
             <h2>Add a new Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="b-title">Blog title:*</label>
-                <input type="text" required id="b-title" />
+                <input
+                    type="text"
+                    id="b-title"
+                    name="title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    required
+                />
                 <label htmlFor="b-body">Blog body:*</label>
-                <input type="text" required id="b-body" />
+                <textarea
+                    name="body"
+                    id="b-body"
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                    required
+                >
+                </textarea>
+                <label htmlFor="b-author">Blog Author</label>
+                <select
+                    name="author"
+                    id="b-author"
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add Blog</button>
+
             </form>
         </div>
     );
