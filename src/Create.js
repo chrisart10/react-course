01fe39d01c05bafe555940514007aadc185696dc
Christian Espinoza <--global>
2022-11-02 15:57:49 -0500

    Addd pendding

--- src/Create.js
+++ src/Create.js
@@ -4,16 +4,19 @@ const Create = () => {
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
     const [author, setAuthor] = useState('mario')
+    const [isPending, SetIsPending] = useState(false)
 
     const handleSubmit = (e) => {
         e.preventDefault()
         const blog = { title, body, author }
+        SetIsPending(true)
         fetch('http://localhost:8000/blogs', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(blog)
         }).then(()=>{
             console.log('New blog added')
+            SetIsPending(false)
         })
     }
     return (
@@ -48,7 +51,8 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Adding blog.... </button>}
 
             </form>
         </div>
