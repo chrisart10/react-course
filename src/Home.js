8e7d39c0b236c2fe836251277138d0beb7e13f44
Christian Espinoza <--global>
2022-11-02 08:47:10 -0500

    Add error Feedback

--- src/Home.js
+++ src/Home.js
@@ -5,23 +5,32 @@ import BlogsList from './Blogs';
 const Home = () => {
     const [blogs, SetBlogs] = useState(null)
     const [isPending, SetIsPending] = useState(true)
+    const [error,setError] = useState(null)
 
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
                 .then(res => {
+                    if(!res.ok){
+                        setError(res.message)
+                        throw Error('Could not fetch data for that resource')
+                    }
                     return res.json()
                 })
                 .then(data => {
-                    console.log(data)
                     SetBlogs(data)
                     SetIsPending(false)
+                })
+                .catch((err)=>{
+                    setError(err.message)
+                    console.log(err.message)
                 })
         }, 1000)
     }, [])
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
             {isPending && <div>Loading ...</div>}
             {blogs && <BlogsList
                 blogs={blogs}
